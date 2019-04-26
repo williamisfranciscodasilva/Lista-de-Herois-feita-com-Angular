bdf1d8f0d8178c7e4d28725a273c70c02653af00
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-04-25 23:06:17 -0300

    Lista de Herois feita com Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Superman"},
+    {id: 2, name: "Batman"},
+    {id: 3, name: "Mulher Maravilha"},
+    {id: 4, name: "Flash"},
+    {id: 5, name: "Aquaman"},
+    {id: 6, name: "Lanterna Verde"},
+    {id: 7, name: "Mulher Gavião"},
+    {id: 8, name: "Caçador Marciano"},
+    {id: 9, name: "Supergirl"},
+    {id: 10, name: "Shazam"}
+];
